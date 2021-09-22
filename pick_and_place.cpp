a8102ea5deccd51f780b1b9bfcb7cb21afb5fb2d
njnovak <71943270+njnovak@users.noreply.github.com>
2021-09-22 17:54:23 -0400

    Update pick_and_place.cpp

--- pick_and_place.cpp
+++ pick_and_place.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+int main(){
+  string box_nums;
+  cout << "Please enter the number of boxes, as numbers spearated by spaces. \n Format: S/M/L/XL: " << endl;
+  cin >> box_nums;
+  
+  return 0;
+}

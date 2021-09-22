5df9b00745ac80f5bc228f2589d721791f601f3b
njnovak <71943270+njnovak@users.noreply.github.com>
2021-09-22 18:05:04 -0400

    Update pick_and_place.cpp

--- pick_and_place.cpp
+++ pick_and_place.cpp
@@ -10,13 +10,13 @@ void getInputs(){
   string box_sizes;
   cout << "Please enter the number of boxes, as numbers spearated by spaces. \n Format: S/M/L/XL: " << endl;
   cin >> box_nums;
-  checkInputs(box_nums, "first, box number");
+  checknegative(box_nums, "first, box number");
   cout << "Please enter the amount of parts each box can hold, as numbers spearated by spaces. \n Format: S/M/L/XL: " << endl;
   cin >> box_sizes;
-  checkInputs(box_nums, "second, box size");
+  checkNegative(box_nums, "second, box size");
 }
 
-void checkInputs(string num_string, string var_name){ //pass by reference here
+void checkNegative(string num_string, string var_name){ //pass by reference here
   for(int i=0; i<num_string.size(); i++){
     if(box_nums[i] == '-'){
       cout << "Negative number detected in the " << var_name << " input. Please re-enter inputs with positive numbers." << endl;
